e94af7d44aea05dea6f6472ea59f14b09e650b77
Colin Frame <cf99@mail.com>
2019-03-05 17:29:37 -0500

    username to email

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -2,14 +2,18 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  username: {
+  email: {
     type: String,
-    required: 'Please provide a username'
+    required: 'Please provide an email'
   },
   password: {
     type: String,
     required: 'Please provide a password'
   },
+  hasapplication: {
+    type: Boolean,
+    required: 'n/a'
+  }
 });
 
 module.exports = mongoose.model('Users', UserSchema);
